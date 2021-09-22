ba91c4a1b48ee38a74a6479b2b381e2069d33f76
Miguel Araujo <miguelant.araujo@gmail.com>
2021-09-22 19:03:08 -0400

    Added Apache 2.0 License

--- release.sh
+++ release.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2021 Miguel Araujo
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -159,7 +173,7 @@ download_chart_releaser() {
 
         curl -s -L --create-dirs -o ./$output_file_name "$DOWNLOAD_URL"
 
-        tar -xf $output_file_name
+        tar -xzf $output_file_name
         rm README.md LICENSE $output_file_name
     fi
 }
